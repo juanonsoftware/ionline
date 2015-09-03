b32bda95e17cfc588a2e6aab445545621fa06d9c
juanonsoftware <juanonsoftware@github.com>
2015-09-03 17:27:34 +0700

    Use Rabbit.Cache.Redis

--- Rabbit.IWasThere.Services/RedisDataService.cs
+++ Rabbit.IWasThere.Services/RedisDataService.cs
@@ -1,6 +1,7 @@
-﻿using Rabbit.Foundation.Data;
+﻿using Rabbit.Cache;
+using Rabbit.Cache.Redis;
+using Rabbit.Foundation.Data;
 using Rabbit.Integrations.Redis;
-using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,31 +10,29 @@ namespace Rabbit.IWasThere.Services
 {
     public class RedisDataService : IDataService
     {
-        private readonly ConnectionMultiplexer _connection;
         private readonly IDataService _directService;
+        private readonly ICache _redisCache;
 
         public RedisDataService(string endPoint, string password)
         {
-            _connection = ConnectionMultiplexerManager.GetCurrent(endPoint, password);
+            var connection = ConnectionMultiplexerManager.GetCurrent(endPoint, password);
+
             _directService = new DirectDataService();
+            _redisCache = new RedisCache(connection.GetDatabase());
         }
 
         public IEnumerable<DataItem> GetCategories(string dataFileUrl)
         {
-            var cache = _connection.GetDatabase();
-            var dataInCache = cache.Get<List<DataItem>>(dataFileUrl);
+            var dataInCache = _redisCache.Get<List<DataItem>>(dataFileUrl);
 
-            if (dataInCache == null)
-            {
-                var categories = _directService.GetCategories(dataFileUrl).ToList();
-                cache.Set(dataFileUrl, categories, TimeSpan.FromHours(1));
-
-                return categories;
-            }
-            else
+            if (dataInCache != null)
             {
                 return dataInCache;
             }
+
+            var categories = _directService.GetCategories(dataFileUrl).ToList();
+            _redisCache.Set(dataFileUrl, categories, TimeSpan.FromHours(1));
+            return categories;
         }
     }
 }
