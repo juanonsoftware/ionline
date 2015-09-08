3d96f9ed9b1ee0773d1ac5dcbd1b37ff18743cfb
juanonsoftware <juanonsoftware@github.com>
2015-09-08 15:53:32 +0700

    requires https only

--- Rabbit.IOnline/App_Start/FilterConfig.cs
+++ Rabbit.IOnline/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Rabbit.IOnline.App_Start
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
