02ef9d0eeffa9366451926074385419f568c69d5
juanonsoftware <juanonsoftware@github.com>
2015-09-11 15:00:08 +0700

    AppHarborHttpsInProductionAttribute

--- Rabbit.IOnline/App_Start/FilterConfig.cs
+++ Rabbit.IOnline/App_Start/FilterConfig.cs
@@ -1,14 +1,33 @@
-﻿using Rabbit.Web.Mvc.Filters;
+﻿using System;
+using Rabbit.Web.Mvc.Filters;
 using System.Web.Mvc;
 
 namespace Rabbit.IOnline.App_Start
 {
+    public class AppHarborHttpsInProductionAttribute : RequireHttpsInProductionAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsSecureConnection)
+            {
+                return;
+            }
+
+            if ("https".Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+    }
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequireHttpsInProductionAttribute());
+            filters.Add(new AppHarborHttpsInProductionAttribute());
         }
     }
 }
