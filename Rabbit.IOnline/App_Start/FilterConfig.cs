cff2d57f6737f4018c5bed23e9fd0b5c774c681a
juanonsoftware <juanonsoftware@github.com>
2015-09-11 15:20:12 +0700

    Update RabbitWebMvc package

--- Rabbit.IOnline/App_Start/FilterConfig.cs
+++ Rabbit.IOnline/App_Start/FilterConfig.cs
@@ -1,33 +1,14 @@
-﻿using System;
-using Rabbit.Web.Mvc.Filters;
+﻿using Rabbit.Web.Mvc.Filters;
 using System.Web.Mvc;
 
 namespace Rabbit.IOnline.App_Start
 {
-    public class AppHarborHttpsInProductionAttribute : RequireHttpsInProductionAttribute
-    {
-        public override void OnAuthorization(AuthorizationContext filterContext)
-        {
-            if (filterContext.HttpContext.Request.IsSecureConnection)
-            {
-                return;
-            }
-
-            if ("https".Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], StringComparison.InvariantCultureIgnoreCase))
-            {
-                return;
-            }
-
-            base.OnAuthorization(filterContext);
-        }
-    }
-
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AppHarborHttpsInProductionAttribute());
+            filters.Add(new RequireHttpsInProductionAttribute(new XForwardedProtoHandleNonHttpsWorker()));
         }
     }
 }
---
+++ Rabbit.IOnline/App_Start/XForwardedProtoHandleNonHttpsWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Rabbit.Web.Mvc.Filters;
+
+namespace Rabbit.IOnline.App_Start
+{
+    public class XForwardedProtoHandleNonHttpsWorker : HandleNonHttpsWorker
+    {
+        public override bool CanValidate(AuthorizationContext filterContext)
+        {
+            if ("https".Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return base.CanValidate(filterContext);
+        }
+    }
+}
