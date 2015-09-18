2e097b4890395f716917b7962e23df4f1baf93d3
juanonsoftware <juanonsoftware@github.com>
2015-09-18 17:21:03 +0700

    Choose ApplicationBootstrap to use

--- Rabbit.IOnline/Global.asax.cs
+++ Rabbit.IOnline/Global.asax.cs
@@ -1,14 +1,36 @@
-﻿using Rabbit.iOnline.Ioc.SimpleInjector;
+﻿using Rabbit.iOnline.Ioc;
+using Rabbit.iOnline.Ioc.Autofac;
 using Rabbit.Web;
+using System;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
 
 namespace Rabbit.IOnline
 {
     public class MvcApplication : CustomHttpApplication
     {
         public MvcApplication()
-            //: base(new ApplicationBehavior(new AutofacApplicationBootstrap()))
-            : base(new ApplicationBehavior(new SimpleInjectorApplicationBootstrap()))
+            : base(new ApplicationBehavior(GetApplicationBootstrap()))
         {
+        }
+
+        private static IApplicationBootstrap GetApplicationBootstrap()
+        {
+            var bootstrapFile = HostingEnvironment.MapPath("~/App_Data/ApplicationBootstrap.txt");
+            if (string.IsNullOrWhiteSpace(bootstrapFile) || !File.Exists(bootstrapFile))
+            {
+                return new AutofacApplicationBootstrap();
+            }
+
+            var typeName = File.ReadAllLines(bootstrapFile).First();
+            var bootstrapType = Type.GetType(typeName);
+            if (bootstrapType == null)
+            {
+                return new AutofacApplicationBootstrap();
+            }
+
+            return (IApplicationBootstrap)Activator.CreateInstance(bootstrapType);
         }
     }
 }
